d86d22f4e5786dd6465b1968a5f0095f2b2767dd
alexpearson340 <alexpearson340@gmail.com>
2025-11-23 14:25:33 +1100

    Move to new repo: server

---
+++ include/snake_server/SnakeServer.h
@@ -0,0 +1,19 @@
+#include <unordered_map>
+#include "engine/Engine.h"
+#include "engine/NetworkLayer.h"
+#include "snake_server/Player.h"
+
+class SnakeServer : public Engine {
+public:
+    SnakeServer(int width, int height) ;
+    
+    void handleInput();
+    void create(); 
+    void update(); 
+    void render(); 
+    void cleanup();
+
+private:
+    NetworkLayer network;
+    std::unordered_map<int, Player> clientIdToPlayerMap;
+}
---
+++ src/snake_server/SnakeServer.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "snake_server/Constants.h"
+#include "snake_server/SnakeServer.h"
+
+SnakeServer::SnakeServer(int width, int height)
+    : Engine(width, height)
+    , network {8170} 
+    , clientIdToPlayerMap {} {
+}
+
+void SnakeServer::handleInput() {
+    std::vector<ClientMessage> messages {network.pollMessages()}; 
+    for (auto msg: messages) {
+        if (msg.data == SnakeConstants::KEY_QUIT) {
+            running = false;
+        }
+        else if (msg.data == SnakeConstants::KEY_UP) {
+            std::cout << "KEY_UP" << std::endl;
+        }
+        else if (msg.data == SnakeConstants::KEY_DOWN) {
+            std::cout << "KEY DOWN" << std::endl;
+        }
+        else if (msg.data == SnakeConstants::KEY_LEFT) {
+            std::cout << "KEY_LEFT" << std::endl;
+        }
+        else if (msg.data == SnakeConstants::KEY_RIGHT) {
+            std::cout << "KEY_RIGHT" << std::endl;
+        }
+        else {
+            std::cout < "Unexpected receive from client (clientid" << msg.clientid << "): " << msg.data << std::endl;
+        }
+    }
+} 
+
+void SnakeServer::create() {
+}
+
+void SnakeServer::update() {
+}
+
+void SnakeServer::render() {
+}
+
+void SnakeServer::cleanup() {
+}
