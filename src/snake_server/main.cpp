ae83e820de24093905058041e4933ec95b9c68af
alexpearson340 <alexpearson340@gmail.com>
2026-07-09 21:53:19 +1000

    Replay: trigger with an env var SNAKE_REPLAY, rather than checking for special file name

--- src/snake_server/main.cpp
+++ src/snake_server/main.cpp
@@ -1,19 +1,20 @@
-#include "common/Constants.h"
 #include "common/Log.h"
 #include "common/MessageLogReader.h"
 #include "snake_server/SnakeServer.h"
 
+#include <cstdlib>
+#include <optional>
+#include <string>
+
 int main() {
     const std::string applicationName {"snake_server"};
-    const std::string replayPath {applicationName + ".replay.jsonl"};
     initLogging(applicationName, false, true);
 
-    // If there is a replay file, read the SERVER_CONFIG
-    // message at the top of the file and use it to seed
-    // the game engine
+    // Replay mode is selected by the SNAKE_REPLAY env var, whose value
+    // is the replay file path. If it is unset, run in normal mode
     std::optional<MessageLogReader> reader;
     std::optional<ProtocolMessage> header;
-    if (std::filesystem::exists(replayPath)) {
+    if (const char * replayPath = std::getenv("SNAKE_REPLAY"); replayPath != nullptr && replayPath[0] != '\0') {
         reader.emplace(replayPath);
         header = reader->first();
     }
